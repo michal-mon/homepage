077cbe60191c3172af2d54faff7c6a0ee1a0ab7e
Michal Mon <mmon1235@gmail.com>
2020-05-04 18:46:39 +0200

    added content for week 4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const name = 'Michał Moń';
+const name = "Michał Moń";
 const age = 19;
-alert('Witam na stronie głównej! Zajrzyj do konsoli ;)');
+alert("Witam na stronie głównej! Zajrzyj do konsoli ;)");
 console.log(`Nazywam się ${name} i mam ${age} lat`);
