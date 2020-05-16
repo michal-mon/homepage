77520f600ae8b8dcc6be67621f59f0ca22b6c400
Michal Mon <mmon1235@gmail.com>
2020-05-16 12:46:48 +0200

    normalize.css, gradient, js function hello, flexbox nav

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const name = "Michał Moń";
+/*const name = "Michał Moń";
 const age = 19;
-console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log(`Nazywam się ${name} i mam ${age} lat`);*/
 
 //const paragraph = document.querySelector('.articles--js');
 //paragraph.innerHTML = 'Mistrz Javascript v2';
 //console.log(paragraph);
+
+const user = {
+    className: "User",
+    nameUser: "Michał",
+    ageUser: 19,
+}
+
+function helloWorld(name, age, className){
+console.log(`Cześć, jestem ${name} i mam ${age} lat, a ta funkcja została wywołana z obiektu klasy ${className}`);
+}
+
+helloWorld(user.nameUser, user.ageUser, user.className);
